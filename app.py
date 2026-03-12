a3df2e4cd455fbc9428f34483142791386663a0b
Vungarala yaswanth <vungaralayaswanth123@gmail.com>
2026-03-12 15:12:53 +0530

    Add expense backend

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+expenses = []
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        amount = request.form["amount"]
+        category = request.form["category"]
+        description = request.form["description"]
+
+        expense = {
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+
+        expenses.append(expense)
+
+        return redirect("/dashboard")
+
     return render_template("index.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", expenses=expenses)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
